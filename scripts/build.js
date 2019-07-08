8444ff62cc5822a05d76f23e32adf1bd6159a0fb
xinghunm <xinghunm@curacloudcorp.com>
2019-07-08 15:53:27 +0800

    add build script

---
+++ scripts/build.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+// eslint-disable-next-line
+const colors = require('colors');
+const config = require('../config');
+
+const compiler = webpack(config);
+
+compiler.run((err, stats) => {
+  if (err) {
+    console.log(err);
+    return 1;
+  }
+  const messages = stats.toJson({ all: false, warnings: true, errors: true });
+  const hasErrors = messages.errors.length;
+  if (hasErrors) {
+    console.log('Errors: '.bold.red);
+    return messages.errors.map(error => console.log(error.red));
+  }
+  const hasWarnings = messages.warnings.length;
+  if (hasWarnings) {
+    console.log('Warnings: '.bold.yellow);
+    return messages.warnings.map(warning => console.log(warning.yellow));
+  }
+  console.log('Compiled successfully.');
+
+  return 0;
+});
