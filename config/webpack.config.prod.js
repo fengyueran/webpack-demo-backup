bba39b1971ba1f700604ff817b06f361ec458f26
xinghunm <xinghunm@curacloudcorp.com>
2019-07-09 13:30:23 +0800

    add terser-webpack-plugin to compress css

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,3 +1,58 @@
+const isWsl = require('is-wsl');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = {
-  mode: 'production' // "production" | "development" | "none"
+  mode: 'production', // "production" | "development" | "none"
+  optimization: {
+    // 定制压缩工具，压缩js
+    minimizer: [
+      new TerserPlugin({
+        // https://github.com/terser-js/terser#minify-options
+        terserOptions: {
+          parse: {
+            // we want terser to parse ecma 8 code. However, we don't want it
+            // to apply any minfication steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            // Specify ECMAScript release: 5, 6, 7 or 8.
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            // Disabled because of an issue with Uglify breaking seemingly valid code:
+            // https://github.com/facebook/create-react-app/issues/2376
+            // Pending further investigation:
+            // https://github.com/mishoo/UglifyJS2/issues/2011
+            comparisons: false,
+            // Disabled because of an issue with Terser breaking valid code:
+            // https://github.com/facebook/create-react-app/issues/5250
+            // Pending futher investigation:
+            // https://github.com/terser-js/terser/issues/120
+            inline: 2
+          },
+          mangle: {
+            safari10: true
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true
+          }
+        },
+        // Use multi-process parallel running to improve the build speed
+        // Default number of concurrent runs: os.cpus().length - 1
+        // Disabled on WSL (Windows Subsystem for Linux) due to an issue with Terser
+        // https://github.com/webpack-contrib/terser-webpack-plugin/issues/21
+        parallel: !isWsl,
+        cache: true,
+        sourceMap: true
+      })
+    ]
+    // Keep the runtime chunk separated to enable long term caching
+    // https://twitter.com/wSokra/status/969679223278505985
+  }
 };
