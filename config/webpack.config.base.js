9b07d582213d12b9f9747af8899160974bd03f99
xinghunm <xinghunm@curacloudcorp.com>
2019-07-09 13:42:10 +0800

    change filename with name and contenthash

--- config/webpack.config.base.js
+++ config/webpack.config.base.js
@@ -1,10 +1,21 @@
 const paths = require('./paths');
+const { isEnvProduction } = require('./util');
 
 module.exports = {
-  entry: paths.appIndexJs,
+  entry: {
+    // string | object | array, // 默认为 'src/index'
+    main: paths.appIndexJs
+  },
   output: {
+    // string, 必须是绝对路径
     path: paths.appBuild,
-    filename: 'bundle.js'
+
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // In development, it does not produce real files.
+    // string，entry为对象时，[name]为entry的key，此时为main
+    filename: isEnvProduction
+      ? 'static/js/[name].[contenthash:8].js'
+      : 'static/js/bundle.js'
   },
   resolve: {
     // 查找自动添加扩展
